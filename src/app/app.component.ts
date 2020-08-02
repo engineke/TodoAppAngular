299408ee0dfa146b61dbfc9faa506a3f1d35fc9f
Engin EKE <engineke@Engin-MacBook-Air.local>
2020-08-02 12:01:42 +0300

    Add item, action change, model bind, display all.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { TodoModel, TodoItem } from './app.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TodoApp';
+  model = new TodoModel();
+  isDisplay = false;
+
+  getUserName() {
+    return this.model.user;
+  }
+
+  getItems() {
+    if (this.isDisplay)
+      return this.model.items;
+      
+    return this.model.items.filter(x => x.action == false);
+  }
+
+  addItem(value) {    
+    if (value != null && value.trim() != '') {
+      this.model.items.push(new TodoItem(value, false));
+    }
+  }
 }
---
+++ src/app/app.model.ts
@@ -0,0 +1,24 @@
+export class TodoModel {
+    user;
+    items;
+    
+    constructor() {
+        this.user = "Engin";
+        this.items = [
+            new TodoItem("Kitap Okumak", true),
+            new TodoItem("Kahvaltı", false),
+            new TodoItem("Spor", false),
+            new TodoItem("Sinema", false)
+        ]
+    }
+}
+
+export class TodoItem {
+    description;
+    action;
+
+    constructor(description, action) {
+        this.description = description;
+        this.action = action;
+    }
+}
